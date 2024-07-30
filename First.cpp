5ae9efee0c9078943e21d75cc6c8cce033f92b2c
Bipin Singh <bipinsingh8052@gmail.com>
2024-07-30 12:50:36 +0530

    first file

---
+++ First.cpp
@@ -0,0 +1,106 @@
+// // Conditional operater
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int maths_marks;
+//     cout<<"Enter the Maths marks:";
+//     cin>>maths_marks;
+
+//     int science_marks;
+//     cout<<"Enter the  Science marks:";
+//     cin>>science_marks;
+
+//     int social_science_marks;
+//     cout<<"Enter the   Social_Science marks:";
+//     cin>>social_science_marks;
+//     int Gk_marks;
+//     cout<<"Enter the   Gk marks:";
+//     cin>>Gk_marks;
+
+//     int English_marks;
+//     cout<<"Enter the  English marks:";
+//     cin>>English_marks;
+
+//     int total=0;
+//     total=maths_marks+English_marks+social_science_marks+science_marks+Gk_marks;
+//      cout<<"THis is total marks"<<total<<"\n";
+//     float percentage=0;
+//     percentage=total/5.0;
+//     cout<<"this is percentage"<<percentage<<"\n";
+//     int count=0;
+//     if(maths_marks>=60){
+//       count++;
+
+//     }
+//     else if(science_marks>=60){
+//       count++;
+
+//     }
+//     else if(social_science_marks>=60){
+//       count++;
+
+//     }
+//     else if(Gk_marks>=60){
+//       count++;
+
+//     } 
+//      else if(English_marks>=60){
+//       count++;
+
+//     }
+//     else{
+//         cout<<"this student is fail";
+//     }
+    
+//     return 0;
+// }
+
+
+// WAP to accept the nuumber by user and check it number is positive ya negative and print it
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//   int number;
+//   cout << "enter the number:";
+//   cin >> number;
+//   if( number > 0 ){
+//     cout << "positive number:";
+//   }
+//   else
+//   {
+//     cout << "negative number";
+//   }
+//   return 0;
+// }
+
+
+
+// WAP accept the number by the user and Whether it this number is divided by 7 if yes then print and also print it this is not divided by 7???? 
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//   int  Number ;
+//   cout << "Enter the number:";
+//   cin >>  Number;
+//   if(Number % 7 == 0)
+//   {
+//     cout << "this number is Divided by 7";
+//   }
+//   else{
+//     cout <<  "This number is not Divided by 7";
+//   }
+// }
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  
+}
