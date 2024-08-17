71f92ac6d677dfad3d7a805edb8fb1745b4c43eb
Bipin Singh <bipinsingh8052@gmail.com>
2024-08-17 11:59:23 +0530

    question

--- ForLoop.cpp
+++ ForLoop.cpp
@@ -1,11 +1,32 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int i =9;
+//     for(;i<11;)
+//     {
+//         cout<<"Hello world";
+//         i++;
+//     }
+// }
+
+
+
 #include<iostream>
 using namespace std;
 int main()
 {
-    int i =9;
-    for(;i<11;)
+    int NumberMax = 0;
+    for(int i=1;i <=5 ;i++)
     {
-        cout<<"Hello world";
-        i++;
+        int Number1;
+        cout<< "Enter the number :";
+        cin >> Number1 ;
+        if(NumberMax < Number1)
+        {
+            NumberMax = Number1;
+        }
+
     }
+    cout << NumberMax;
 }
