b4d271b663ad87689843d344acf41508e04c6a72
Bipin Singh <bipinsingh8052@gmail.com>
2024-08-23 10:37:07 +0530

    secodn

--- Patten_printing.cpp
+++ Patten_printing.cpp
@@ -74,20 +74,48 @@
 
 
 
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a=5;
+//     for(int i=1;i<=a;i++)
+//     {
+//         for(int j =a ;j >=i ;j--)
+//         {
+//             cout <<" ";
+//         }
+//         for(int k=1 ;k<=i ;k++)
+//         {
+//             cout<< "*";
+//         }
+//         cout <<"\n";
+//     }
+//     return 0;
+// }
+
+
+
 #include<iostream>
 using namespace std;
 int main()
 {
-    int a=5;
-    for(int i=1;i<=a;i++)
+    for(int i=1;i<=5;i++)
     {
-        for(int j =a ;j >=i ;j--)
+        for (int j=1 ;j<=9 ;j++)
         {
-            cout <<" ";
-        }
-        for(int k=1 ;k<=i ;k++)
-        {
-            cout<< "*";
+
+            if(i==1|| j==5 || 
+            (i==2&&j==2)||(i==2 && j==3)||(i==2&& j==4)||(i==2&& j==6)||(i==2&& j==7)||(i==2&& j==8)||
+            (i==3&& j==3)||(i==3&& j==4)||(i==3&& j==6)||(i==3&& j==7)||
+            (i==4&& j==4)||(i==4&& j==6))
+            {
+                cout<<"*";
+            }
+
+            else{
+                cout<<" ";
+            }
         }
         cout <<"\n";
     }
