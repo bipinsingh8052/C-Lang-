369e1a08fbd50a1416106da1042ae9e79f5fdc0e
Bipin Singh <bipinsingh8052@gmail.com>
2024-09-30 12:49:41 +0530

    question

--- Switch-case.cpp
+++ Switch-case.cpp
@@ -162,36 +162,17 @@
 //     int a;
 //     cout<<"Enter the hour:";
 //     cin>>a;
-//     switch(a)
+//     switch(a/12)
 //     {
-//         case 1:
-//         case 2:
-//         case 3:
-//         case 4:
-//         case 5:
-//         case 6:
-//         case 8:
-//         case 7:
-//         case 9:
-//         case 10:
-//         case 11:
+//        
+//       case 0:
 //         {
 //             cout <<"AM"<<a;
 //             break;
 //         }
-//         case 12:
-//         case 21:
-//         case 13:
-//         case 14:
-//         case 15:
-//         case 16:
-//         case 17:
-//         case 18:
-//         case 19:
-//         case 20:
-//         case 22:
-//         case 23:
-//         case 24:{
+//        
+//         case 1:
+//         {
 //             cout<< "P M "<<a;
 //             break;
 //         }
