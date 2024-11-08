3ef1ea881589e5403500b56e45b6ab4495a3e352
Bipin Singh <bipinsingh8052@gmail.com>
2024-11-08 13:25:50 +0530

    mer

--- array/arra.cpp
+++ array/arra.cpp
@@ -1,8 +1,21 @@
 // [] supscript 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a[5][2]={{1,2},{3,1},{3,1},{3,1},{3,1}};
+//     cout<<sizeof(a);
+    
+// }
+
+
 #include<iostream>
 using namespace std;
 int main(){
-    int a[5][2]={{1,2},{3,1},{3,1},{3,1},{3,1}};
-    cout<<sizeof(a);
+    int a[]={1,23,13,3,13,1};
+    cout<<sizeof(a)/sizeof(a[0])<<"\n";
+    char b[]={'a','b','k'};
+    cout<<sizeof(b)/sizeof(b[0])<<"\n";
+    int size=sizeof(b)/sizeof(b[0]);
+    cout<<size;
     
 }
