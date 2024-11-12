4d8807264671deca4a41448f5bc2bc833dff259e
Bipin Singh <bipinsingh8052@gmail.com>
2024-11-12 13:37:40 +0530

    min and max  question answer and even and odd

--- array/arra.cpp
+++ array/arra.cpp
@@ -50,16 +50,71 @@
 
 
 
-// w.a.p to display the sum of the values of array
+// // w.a.p to display the sum of the values of array
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a[]={1,2,3,4,5};
+//     int sum=0;
+//     int siz=sizeof(a)/sizeof(a[0]);
+//     for(int i=0;i<= siz-1; i++){
+//         sum=sum+a[i];
+//     }
+//     cout << sum;
+// }
+
+
+
+
+// // find the min value in the array
+// find the max number in the given array
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a[]={1,2,3,4,5};
+//     // int min=a[0];
+//     // int max=a[0];
+//     int min = INT8_MAX;
+//     int max =INT16_MIN;
+//     int siz=sizeof(a)/sizeof(a[0]);
+//     for(int i=0;i<= siz-1; i++){
+//         if(min>a[i])
+//         {
+//           min=a[i];
+//         }
+//         if(max<a[i]){
+//           max=a[i];
+//         }
+//     }
+//     cout<<max<<"max value"<<"\n";
+//     cout<<min<<"min value"<<"\n";
+// }
+
+
+
+
+// // find the even number in the array
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   int a[]={1,2,3,3,4,4,55,6,7,7,8,8,9,9,90};
+//   int len =sizeof(a)/sizeof(a[0]);
+//   for(int i=0;i<=len-1;i++){
+//     if(a[i]%2==0){
+//       cout<<a[i]<<"\t";
+//     }
+//   }
+// }
+
+
+
 #include<iostream>
 using namespace std;
-int main()
-{
-    int a[]={1,2,3,4,5};
-    int sum=0;
-    int siz=sizeof(a)/sizeof(a[0]);
-    for(int i=0;i<= siz-1; i++){
-        sum=sum+a[i];
-    }
-    cout << sum;
+int main(){
+  int a[]={1,2,3,4,22,221,1,4444,1,445,6543,7654,7654,345,65,234,65};
+  int len =sizeof(a)/sizeof(a[0]);
+  for(int i=0;i<=len-1;i++)
+  return 0;
 }
