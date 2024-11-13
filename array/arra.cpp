d554a742d446f7e709e0418b0b29eaec104ee93a
Bipin Singh <bipinsingh8052@gmail.com>
2024-11-13 16:27:54 +0530

    prime number and second largest number and replace

--- array/arra.cpp
+++ array/arra.cpp
@@ -105,16 +105,92 @@
 //     if(a[i]%2==0){
 //       cout<<a[i]<<"\t";
 //     }
+      // if(a[i]%2 !=0){
+      //   cout<<a[i]<<"\t";
+      // }
 //   }
 // }
 
 
+// // find the second largest number in the array
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   int a[]={1111111,2,3,4,22,221,1,44,1,4,65,54,7654,345,65,234,65};
+//   int len =sizeof(a)/sizeof(a[0]);
+//   int max1,max2;
+//   max1=max2=INT8_MIN;
+//   for(int i=0;i<=len-1;i++)
+//   {
+//     if(a[i]>max1){
+//       max2=max1;
+//       max1=a[i];
+//     }
+//     else if(a[i]>max2 && a[i]<max1){
+//       max2=a[i];
+//     }
+//   }
+//   cout<<max2;
+//   return 0;
+// }
 
+
+// // Find the Prime number in the array
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   int a[]={4,7,6,11,5,1};
+//   int len =sizeof(a)/sizeof(a[0]);
+//   for(int i=0;i<=len-1;i++){
+//     int count=0;
+//     for(int j=1;j<=a[i];j++){
+//       if(a[i]%j==0){
+//         count++;
+//       }
+//       if(a[i]==1){
+//         count=2;
+//       }
+//     }
+//     if(count==2){
+//       cout<<"Prime number :"<<a[i]<<"\n";
+//     }
+//   }
+// }
+
+
+// // W.A.P to Replace the 1 with 0 in the given array?
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   int a[]={2,1,3,1,5,1};
+//   int len =sizeof(a)/sizeof(a[0]);
+//   for(int i=0;i<=len-1;i++){
+//     if(a[i]==1){
+//       a[i]=0;
+//     }
+//     cout<<a[i];
+//   }
+// }
+
+// W.a.p To sift all Zero at the last by holding bill position in array?
 #include<iostream>
 using namespace std;
 int main(){
-  int a[]={1,2,3,4,22,221,1,4444,1,445,6543,7654,7654,345,65,234,65};
+  int a[]={2,0,7,0,3,0,6};
   int len =sizeof(a)/sizeof(a[0]);
-  for(int i=0;i<=len-1;i++)
-  return 0;
+  int len2=len;
+  int b[len2]={};
+  for(int i=0;i<=len-1;i++){
+    for(int j=0;j<=i;j++){
+      if(a[i]==0){
+        b[len2-1]=a[i];
+      }
+      else{
+        b[len2]=a[i];
+      }
+    }
+  }
+  for(int i=0;i<=len2;i++){
+    cout<<b[i];
+  }
 }
