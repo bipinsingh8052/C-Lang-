9371fea4b5f977170a5ccf3f3bae48776c4a229a
Bipin Singh <bipinsingh8052@gmail.com>
2024-11-19 16:45:50 +0530

    find the transpose matrix problem

--- array/2d_Array.cpp
+++ array/2d_Array.cpp
@@ -109,29 +109,116 @@
 // }
 
 
-// w.a.p to find the second largest value in the array
+// // w.a.p to find the second largest value in the array
+// #include<iostream>
+// using namespace std;
+// int main(){
+//      int a[3][3];
+//     for(int i=0;i<3;i++){
+//         for(int j=0;j<3;j++){
+//             cout<<"Enter the number:";
+//             cin>>a[i][j];
+//         }
+//     }
+//     int max1,max2;
+//     max1=max2=INT16_MIN;
+//     for(int i=0;i<3;i++){
+//         for(int j=0;j<3;j++){
+//             if(max1<a[i][j]){
+//                 max2=max1;
+//                 max1=a[i][j];
+//             }
+//             else if(a[i][j]>max2&a[i][j]<max1)
+//             {
+//                 max2=a[i][j];
+//             }
+//         }
+//     }
+//     cout<<max1<<"n"<<max2;
+// }
+
+
+
+// // w.a.p to find the transpose conceft of apply in max;
+// #include<iostream>
+// using namespace std;
+// int main(){
+//          int a[2][3];
+//     for(int i=0;i<2;i++){
+//         for(int j=0;j<3;j++){
+//             cout<<"Enter the number:";
+//             cin>>a[i][j];
+//         }
+//     }
+//     cout<<"OUtput"<<"\n";
+//     for(int i=0;i<2;i++){
+//         for(int j=0;j<3;j++){
+//             cout<<a[i][j]<<"\t";
+//         }
+//         cout<<"\n";
+//     }
+//     cout<<"Data output:"<<"\n";
+//     for(int i=0;i<3;i++){
+//         for(int j=0;j<2;j++){
+//             cout<<a[j][i]<<"\t";
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+
+// second option of this question:
 #include<iostream>
 using namespace std;
 int main(){
-     int a[3][3];
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            cout<<"Enter the number:";
-            cin>>a[i][j];
-        }
-    }
-    int max1,max2;
-    max1=max2=INT16_MIN;
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            if(max1<a[i][j]){
-                max2=max1;
-                max1=a[i][j];
+    int s=3;
+    int l=3;
+    // cout<<"Enter the size of 1d array:"<<"\n";
+    // cin>>s;
+    // cout<<"Enter the size of second 2d index array:"<<"\n";
+    // cin>>l;
+    // cout<<"Enter the  "<<s*l<<" numbers";
+    int a[3][3]={1,2,3,4,5,6,7,8,9};
+    // for(int i=0;i<s;i++){
+    //     for(int j=0;j<l;j++){
+    //         cin>>a[i][j];
+    //     }
+    // }
+
+    for(int i=0;i<s;i++){
+            for(int j=0;j<l;j++){
+                cout<<a[i][j]<<"\t";
             }
-            else if(a[i][j]<max1&a[i][j]>max2);
-            {
-                max2=a[i][j];
-            }
+            cout<<"\n";
         }
-    }
+     cout<<"THis output is given your array:"<<"\n";   
+
+    // if(s==l){
+        
+    
+    // }
+    // else{
+    //     cout<<"this is not a square form:";
+    // }
+
+
+    for(int i=0;i<s;i++){
+        for(int j=0;j<l;j++){
+            int temp =a[i][j];
+            a[i][j]=a[j][i];
+            a[j][i]=temp;
+            cout<<a[i][j]<<"\t";
+        }
+        cout<<"\n";
+        }
+        cout<<"out"<<"\n";
+
+
+    for(int i=0;i<s;i++){
+            for(int j=0;j<l;j++){
+                cout<<a[i][j]<<"\t";
+            }
+            cout<<"\n";
+        }
+    
 }
