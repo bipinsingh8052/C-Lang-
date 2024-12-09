e030815f63b03d76487755fa183170f161215ba9
Bipin Singh <bipinsingh8052@gmail.com>
2024-12-09 16:09:07 +0530

    all completed the doute of pointer function and pointer

--- pointer/Using_Funtion_In_Pointer.cpp
+++ pointer/Using_Funtion_In_Pointer.cpp
@@ -1,10 +1,24 @@
 #include<iostream>
 using namespace std;
 int axia(int u ,int n){
-    
+    return 500;
+}
+int sbi(int u,int n){
+    return 800;
 }
 int main(){
-    int a=99;
-    int b=999;
+    // this is pointer  function  
+    int (*Atm) (int ,int);
+    // int     (depend on the function return );
+    // (*ATM)   (it is function name);
+    // (int ,int) (this is pointer function passing a argument you cant write it variable name)
+
+
+    Atm=sbi;
+    // in this line you can assign the normal function in pointer function
+    int balance=Atm(9,6);
+    cout<<"Amount:"<<balance<<"\n";
+    int balance_second=Atm(9,7);
+    cout<<"Amount second"<<balance_second<<"\n";
 
 }
