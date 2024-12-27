1553b8fa309d260e54b7438ed846b281df299e11
Bipin Singh <bipinsingh8052@gmail.com>
2024-12-27 16:46:47 +0530

    Operator overloading two type one is Unary second is binary

--- OOPS/Operating_overloading.cpp
+++ OOPS/Operating_overloading.cpp
@@ -1,31 +1,55 @@
+// What is Opertor Overloading
+// when you define nature opertor in non-primitive data type(user define data type )  when you use it is called opertor Overloading 
+
+// there are two type of Opertor Overloading
+
+// 1.Unary opertor Overloading (where you can apply it ++,-- like this )
+// 2. binary operator Overloading
+
+
+// // Example of Unary operator Overloading
+
+// #include<iostream>
+// using namespace std;
+// class top
+// {
+//     int a=10;
+//     public:operator++(){
+//         a++;
+//         cout<<"a:"<<a<<"\n";
+//     };
+//     public:operator--(){
+//         a--;
+//         cout<<"a:"<<a<<"\n";
+//     }
+
+// };
+// int main(){
+//     top p;
+//     ++p;
+//     --p;
+// }
+
+
+
+// BInary Opreator Overloading
+
+// W.a.p to find the How much Complex sum
 #include<iostream>
 using namespace std;
-class top
-{
-    int a=10;
-    public:operator++(){
-        a++;
-        cout<<"a:"<<a<<"\n";
-    };
-    public:operator--(){
-        a--;
+class Top{
+    int a=1000;
+    public: Top(int b ){
+        a=b;
+    }
+    operator-(Top & obj){
+        a=obj.a-a;
         cout<<"a:"<<a<<"\n";
     }
-
 };
-    // Operating_overloading(/* args */);
-    // ~Operating_overloading();};
-
-// Operating_overloading::Operating_overloading(/* args */)
-// {
-// }
-
-// Operating_overloading::~Operating_overloading()
-// {
-// }
-
 int main(){
-    top p;
-    ++p;
-    --p;
+    Top a1(1200);
+    Top a2(2000);
+    a1-(a2);
+
 }
