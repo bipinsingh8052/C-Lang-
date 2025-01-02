1b727c8c06ba79b7a10865f324edb953ba7ae77c
Bipin Singh <bipinsingh8052@gmail.com>
2025-01-02 16:17:40 +0530

    Multipal inheritance and multilevel Inheritance

--- OOPS/INHERITANCE.cpp
+++ OOPS/INHERITANCE.cpp
@@ -4,37 +4,136 @@
 
 
 
-// Example of single inHeritance 
+// // Example of single inHeritance 
+// #include<iostream>
+// using namespace std;
+
+// class RBI{
+// int t;
+// public:RBI(){
+//     cout<<"\nRBI memory\n";
+// }
+// ~RBI(){
+//     cout<<"\nRBI memory realeased";
+// }
+// public:void show(){
+//     cout<<"\nRBI class\n";
+// }
+
+// };
+// class SBI:public RBI{
+// int a;
+// public:SBI(){
+//     cout<<"\nsbi memory";
+// }
+// ~SBI(){
+//     cout<<"\nsbi memory released\n";
+// }
+// public: void show(){
+// cout<<"\n sbi class\n";
+// }
+// };
+// int main(){
+//     SBI s;
+//     s.show();
+//     cout<<"size of sbi="<<sizeof(s);
+// }
+
+
+// Example of multilevel inheritance
+
+// #include<iostream>
+// using namespace std;
+
+// class RBI{
+//     int t;
+//     public:RBI(){
+//         cout<<"\nRBI memory\n";
+//     }
+//     ~RBI(){
+//         cout<<"\nRBI memory realeased";
+//     }
+//     public:void show(){
+//         cout<<"\nRBI class\n";
+//     }
+
+// };
+// class SBI:public RBI{
+//     int a;
+//     public:SBI(){
+//         cout<<"\nsbi memory";
+//     }
+//     ~SBI(){
+//         cout<<"\nsbi memory released\n";
+//     }
+//     public: void show(){
+//     cout<<"\n sbi class\n";
+//     }
+// };
+// class Costmor:public SBI {
+//     int b;
+//     public: Costmor (){
+//         cout<<"mermory "<<"\n";
+//     }
+//     ~Costmor(){
+//         cout<<"delete"<<"\n";
+//     }
+// };
+// int main(){
+//     Costmor s;
+//     s.show();
+//     cout<<"size of sbi="<<sizeof(s);
+// }
+
+
+
+
+
+// Example of Multipal inheritance
+
 #include<iostream>
 using namespace std;
 
 class RBI{
-int t;
-public:RBI(){
-    cout<<"\nRBI memory\n";
-}
-~RBI(){
-    cout<<"\nRBI memory realeased";
-}
-public:void show(){
-    cout<<"\nRBI class\n";
-}
-
+    int t;
+    public:RBI(){
+        cout<<"\nRBI memory\n";
+    }
+//    When same name function in class is called SIGNATUR .
+// IS ASLO CALLED A DIMANOND PROBLEM
+    public:void show(){
+        cout<<"\nRBI class\n";
+    }
+     ~RBI(){
+        cout<<"\nRBI memory realeased";
+    }
 };
-class SBI:public RBI{
-int a;
-public:SBI(){
-    cout<<"\nsbi memory";
-}
-~SBI(){
-    cout<<"\nsbi memory released\n";
-}
-public: void show(){
-cout<<"\n sbi class\n";
-}
+class SBI{
+    int a;
+    public:SBI(){
+        cout<<"\nsbi memory";
+    }
+    ~SBI(){
+        cout<<"\nsbi memory released\n";
+    }
+    public: void show(){
+    cout<<"\n sbi class\n";
+    }
+};
+class Costmor:public SBI ,public RBI {
+    int b;
+    public: Costmor (){
+        cout<<"mermory "<<"\n";
+    }
+    ~Costmor(){
+        cout<<"delete"<<"\n";
+    }
 };
 int main(){
-    SBI s;
-    s.show();
-    cout<<"size of sbi="<<sizeof(s);
+    Costmor s;
+
+    
+    // is called dimanod problem
+    // s.show();
+    cout<<"size of sbi="<<sizeof(s)<<"\n";
 }
